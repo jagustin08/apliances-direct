ee4bee47a715a3016257d450a08f50778c766d13
Jayson Agustin <dev.jagustin@gmail.com>
2024-01-15 22:40:34 +0800

    Add splash screen

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,33 @@
 $(document).ready(function() {
     // get started
 
-    $('[data-toggle="tooltip"]').tooltip('hidden');
+    // Tooltip configuration
+    tippy.setDefaultProps({
+        allowHTML: true,
+        hideOnClick: true,
+        // trigger: 'click',
+        placement: 'top-end',
+        // offset: [5, 10],
+        maxWidth: 187,
+        theme: 'white',
+        animation: 'shift-away',
+        zIndex: 1020,
+    });
+
+    tippy('[data-tippy-content]');
+    tippy('[data-template]', {
+        content(reference) {
+        const id = reference.getAttribute('data-template');
+        const template = document.getElementById(id);
+        template.style.display = 'block';
+        return template;
+        },
+    });    
+
+    $('body').css({ overflow: 'hidden', });
+
+    setTimeout(function() {
+        $('.preloader-container').fadeOut('slow');
+        $('body').css({ overflow: 'auto', });
+    }, 3000);
 });
